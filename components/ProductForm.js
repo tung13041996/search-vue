76544dea937a2154d916378f9cbf2f5640b42a2b
Tung Nguyen <nguyenthanhtungbk96@gmail.com>
2022-11-14 15:55:26 +0700

    :lipstick: build search function without component

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,20 +1,8 @@
 app.component('product-form', {
-    template:
-    `<div class="product-search margin-bottom-80px">
-            <form class="ps-relative">
-                <label>
-                    <input type="text" placeholder="Search">
-                    <input type="submit" value="Submit" class="btn_primary btn_submit txt_center ps-absolute t0 r0 b0">
-                </label>
-            </form>
-        </div>
-    `,
+    template: ``,
     data() {
-        return {
-
-        }
+        return {}
     },
-    methods: {
+    methods: {},
 
-    }
 })
--- components/ProductSell.js
+++ components/ProductSell.js
@@ -1,17 +1,12 @@
 app.component('product-sell', {
-    template:
-        `<div class="product-sell">
-                <div class="product-sell__inner">
-                    <div class="product-sell__title txt_center margin-bottom-40px"><h4>All product chosen</h4></div>
-                    <ul>
-
-                    </ul>
-                </div>
-            </div>
-`,
-    data() {
-        return {
-
+    props: {
+        productChoose: {
+            type: Array,
+            required: true
         }
+    },
+    template: ``,
+    data(){
+        return {}
     }
 })
